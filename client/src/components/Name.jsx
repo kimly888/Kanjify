b949fb0ddbee8fa0bf68ed2eb2c2ff0fad16b536
Kim <kimly.jet@gmail.com>
2022-08-03 17:04:49 +0900

    Style: Add box shadow, remove borders

--- client/src/components/Name.jsx
+++ client/src/components/Name.jsx
@@ -1,8 +1,10 @@
+import "./Name.css";
+
 const Name = () => {
   return (
     <form action="localhost:4000/api/getKanji" method="GET">
       <input type="text" name="name" id="name" />
-      <button type="submit">GO</button>
+      <button for="name" type="submit">GO</button>
     </form>
   );
 };
