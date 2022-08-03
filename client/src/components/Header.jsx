52e54b4d6bce8ad823585136c6dc14e1865cdad6
Kim <kimly.jet@gmail.com>
2022-08-03 15:31:16 +0900

    Style: Add padding and flex props to kanji description

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Header.css";
 import Name from "./Name";
-import Submit from "./Submit";
 
 const Header = () => {
   return (
     <header>
       <h1 className="title">Kanjify</h1>
       <Name />
-      <Submit />
     </header>
   );
 };
