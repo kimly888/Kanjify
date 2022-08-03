a64126bb34a4ba8364ccaadc202ad84cb373f522
Kim <kimly.jet@gmail.com>
2022-08-03 14:54:54 +0900

    Feat: Add text input and submit button

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+
 const HOME_API = "http://localhost:4000/";
 
 function App() {
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.css";
+import Name from "./Name";
+import Submit from "./Submit";
 
 const Header = () => {
   return (
     <header>
       <h1 className="title">Kanjify</h1>
+      <Name />
+      <Submit />
     </header>
   );
 };
