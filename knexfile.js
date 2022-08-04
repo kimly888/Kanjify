26a7a62ec9f22e47de04d23fc734c5c4d8009e97
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-04 11:22:23 +0900

    Feat: Server can manipulate database

--- knexfile.js
+++ knexfile.js
@@ -6,18 +6,37 @@ require("dotenv").config({
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: "postgresql",
-  connection: process.env.DB_URL || {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_NAME || "kanjify",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    }
   },
-  migrations: {
-    directory: "./db/migrations",
-  },
-  seeds: {
-    directory: "./db/seeds",
-  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
+const knex = require('../db/knex');
 
 app.use(express.json()); //req.body
 app.use(cors());
@@ -10,9 +11,14 @@ app.get("/api/getKanji", (req, res) => {
 
 })
 
-app.post("/api/kanji", (req, res) => {
-  const { kanji } = req.body;
-  console.log(req.body);
+app.get("/api/kanji", async (req, res) => {
+  const kanji = req.body;
+  console.log(kanji);
+  await knex("users").insert({username: "foo"})
+  // const selected = await knex.select().from("users");
+  // console.log(selected)
+  res.send("test")
+  // const id = await database("users").insert(kanji, 'id');
 })
 
 const PORT = process.env.PORT || 4000;
