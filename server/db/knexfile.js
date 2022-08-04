2e69b5c6969838b2522c3703c7695b4017bd1dc1
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-04 17:07:23 +0900

    Chore: Delete dot env

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,27 +1,16 @@
-require("dotenv").config({
-  path: "./.env.local",
-});
-
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: process.env.DB_HOST || "127.0.0.1",
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME || "kanjify",
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      charset: 'utf8'
+      database: "kanjify",
+      user:   "mutokurumi",
+      password: "kanjifydb"
     },
-    migrations: {
-      directory: "./migrations",
-    },
-    seeds: {
-      directory: "./seeds",
-    }
+    migrations: { directory: "./migrations" },
+    seeds: { directory: "./seeds" },
   },
 
   production: {
