11d5cc7bdb59bfe7648976775b8d71b096f3d49d
cskejivic <pime.fjh.dct.vic1@gmail.com>
2022-08-04 11:45:01 +0900

    fix: change http method from POST to GET

--- client/src/components/Name.jsx
+++ client/src/components/Name.jsx
@@ -2,16 +2,15 @@ import React, { useState } from "react";
 
 const Name = () => {
   const [name, setName] = useState("");
-  const onSubmitForm = async e => {
-    e.preventDefault();
+  const onSubmitForm = async event => {
+    event.preventDefault();
     try {
-      const body = { name };
-      const response = await fetch("http://localhost:4000/api/kanji", {
-        method: "POST",
+      const response = await fetch(`http://localhost:4000/api/kanji/?input=${name}`, {
+        method: "GET",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
       });
-      
+      const obj = await response.json();
+      console.log(obj);
     } catch (err) {
       console.error(err.message);
     }
@@ -19,7 +18,7 @@ const Name = () => {
 
   return (
     <form onSubmit={onSubmitForm} method="GET">
-      <input type="text" name="name" id="name" onChange={e => setName(e.target.value)}/>
+      <input type="text" name="name" id="name" onChange={event => setName(event.target.value)}/>
       <button type="submit">GO</button>
     </form>
   );
