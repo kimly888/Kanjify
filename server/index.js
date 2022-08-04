358b1de469ef1fb5f65e18489acdc4297652a599
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-04 18:43:22 +0900

    Chore: Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const knex = require('./db/knex');
 const path = require('path');
 
 
-app.use(express.static("client"));
+app.use(express.static("../client"));
 
 app.use(express.json()); //req.body
 app.use(cors());
@@ -23,27 +23,27 @@ app.get("/api/kanji/", async (req, res) => {
   const katakana = wanakana.toKatakana(romajiName);
   const katakanaAry = katakana.split('');
 
-  // const kanjiData = await fetch("https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/",{
-  //   method: 'GET',
-  //   params: {on: `${katakanaAry[0]}`},
-  //   headers: {
-  //     'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
-  //     'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
-  //   }
-  // });
+  const kanjiData = await fetch("https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/",{
+    method: 'GET',
+    params: {on: `${katakanaAry[0]}`},
+    headers: {
+      'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
+      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    }
+  });
 
-  // const kanji = await kanjiData.json();
-  // const singleKanji = kanji[22].kanji.character;
+  const kanji = await kanjiData.json();
+  const singleKanji = kanji[22].kanji.character;
 
-  // const kanjiDetailData = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/kanji/${singleKanji}`, {
-  //   headers: {
-  //     'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
-  //     'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
-  //   }
-  // })
+  const kanjiDetailData = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/kanji/${singleKanji}`, {
+    headers: {
+      'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
+      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+    }
+  })
 
-  // const kanjiDetailObj = await kanjiDetailData.json();
-  // const meaning = kanjiDetailObj.kanji.meaning.english;
+  const kanjiDetailObj = await kanjiDetailData.json();
+  const meaning = kanjiDetailObj.kanji.meaning.english;
 
   // Sample Data to check if data can be sent to the front
   let resultObj = [
@@ -72,7 +72,7 @@ app.get("/api/kanji/", async (req, res) => {
   res.status(200).send(JSON.stringify({resultObj}));
 })
   
-app.get("/api/kanji", async (req, res) => {
+app.get("/api/kanjiDB", async (req, res) => {
   const kanji = req.body;
   console.log(kanji);
   await knex("kanji").insert({kanji: "区理酢", furigana: "くりす", romaji: "kurisu"})
@@ -81,7 +81,7 @@ app.get("/api/kanji", async (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "client"));
+  res.sendFile(path.join(__dirname, "../client"));
 });
 
 const PORT = process.env.PORT || 4000;
