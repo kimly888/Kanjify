f551102465f3c239447f68acab077c0b850ca840
cskejivic <pime.fjh.dct.vic1@gmail.com>
2022-08-04 17:34:11 +0900

    feat: Make Functions to access to the API

--- server/index.js
+++ server/index.js
@@ -23,27 +23,99 @@ app.get("/api/kanji/", async (req, res) => {
   const katakana = wanakana.toKatakana(romajiName);
   const katakanaAry = katakana.split('');
 
-  const kanjiData = await fetch("https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/",{
-    method: 'GET',
-    params: {on: `${katakanaAry[0]}`},
-    headers: {
-      'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
-      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+
+  function shuffleArray(array) {
+    const cloneArray = [...array];
+
+    for (let i = cloneArray.length - 1; i >= 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      // change the element of order in array
+      let tmpStorage = cloneArray[i];
+      cloneArray[i] = cloneArray[rand];
+      cloneArray[rand] = tmpStorage;
     }
-  });
 
-  const kanji = await kanjiData.json();
-  const singleKanji = kanji[22].kanji.character;
+    return cloneArray;
+  }
 
-  const kanjiDetailData = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/kanji/${singleKanji}`, {
-    headers: {
-      'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
-      'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+  
+
+  async function singleKanjiFuncAry(katakanaAry) {
+    let result = [];
+
+    for (let katakana of katakanaAry) {
+      const kanjiData = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?on=${katakana}`,{
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
+          'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+        }
+      });
+
+      const kanji = await kanjiData.json();
+      const shuffleKanji = shuffleArray(kanji);
+      const selectedShuffleKanji = shuffleKanji.slice(0, 3);
+
+      for (let singleKanji of selectedShuffleKanji) {
+        result.push(singleKanji.kanji.character);
+      }
+
     }
-  })
 
-  const kanjiDetailObj = await kanjiDetailData.json();
-  const meaning = kanjiDetailObj.kanji.meaning.english;
+    return result;
+  }
+
+  const selectedKanjiAry = await singleKanjiFuncAry(katakanaAry);
+  console.log(selectedKanjiAry);
+
+  async function getKanjinMeaning(selectedKanjiAry) {
+    let result = [];
+
+    for (let kanji of selectedKanjiAry) {
+      const kanjiMeaningData = await fetch(`https://kanjialive-api.p.rapidapi.com/api/public/kanji/${kanji}`,{
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'fb204f9fc6msh5fe937040b3b47fp1ec3cfjsn0a6c2ab1a027',
+          'X-RapidAPI-Host': 'kanjialive-api.p.rapidapi.com'
+        }
+      });
+
+      const kanjiDetailObj = await kanjiMeaningData.json();
+      const meaning = kanjiDetailObj.kanji.meaning.english;
+     
+      result.push(meaning);
+
+    }
+
+    return result;
+  }
+
+  const meaningAry = await getKanjinMeaning(selectedKanjiAry);
+  console.log(meaningAry);
+
+  let finalResult = []
+
+  // create 3 candidate object
+  for(let i=0; i<3; i++) {
+    let tempObj = {
+      kanjiName: "",
+      eachKanji: []
+    }
+
+    finalResult.push(tempObj);
+  }
+
+  // create the number of eachKanji object depending on the number of kanjiName 
+  for(let i=0; i<katakanaAry.length; i++) {
+    let tempObj = {
+      character: "",
+      hiragana: "",
+      meaning: ""
+    }
+
+    finalResult[i].eachKanji.push(tempObj);
+  }
+
 
   // Sample Data to check if data can be sent to the front
   let resultObj = [
@@ -51,17 +123,17 @@ app.get("/api/kanji/", async (req, res) => {
       kanjiName: "区理酢",
       eachKanji: [
         {
-          kanji: "区",
+          character: "区",
           hiragana: "く",
           meanings: "district"
         },
         {
-          kanji: "理",
+          character: "理",
           hiragana: "り",
           meanings: "science"
         },
         {
-          kanji: "酢",
+          character: "酢",
           hiragana: "す",
           meanings: "vinegar"
         }
