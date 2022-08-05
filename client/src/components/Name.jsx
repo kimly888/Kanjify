88665913920bf355df5022abb35d275b03a76e2e
Kurumi <90857923+walnut07@users.noreply.github.com>
2022-08-05 19:58:19 +0900

    Update Name.jsx

--- client/src/components/Name.jsx
+++ client/src/components/Name.jsx
@@ -8,7 +8,7 @@ const Name = ({ setIsActive, setKanjiData }) => {
     event.preventDefault();
     try {
       const response = await fetch(
-        https://kanjify-server.herokuapp.com/api/kanji/input=${name}`,
+        `https://kanjify-server.herokuapp.com/api/kanji/input=${name}`,
         {
           method: "GET",
           headers: { "Content-Type": "application/json" },
