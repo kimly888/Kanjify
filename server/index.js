1bfd4d2df3b013f8d477f1d32bd08975d6a619ca
Kim <kimly.jet@gmail.com>
2022-08-05 15:14:13 +0900

    Fix: Send data only if http request suceeds

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get("/api/kanji/", async (req, res) => {
   const kanjiDefinitions = combiner(generatedDefinitionObj);
   const kanjiData = getKanjiData(hiraganaArr, kanjiNames, kanjiDefinitions);
 
-  res.send(kanjiData);
+  res.status(200).send(kanjiData);
 });
 
 // app.get("/api/kanjiDB", async (req, res) => {
