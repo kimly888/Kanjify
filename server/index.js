22afa9a9bf9fa7db220637b5a2d26000f862c4bc
Kurumi <p20080mk@gm.tsuda.ac.jp>
2022-08-07 22:13:17 +0900

    Fix: Handle empty submit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const wanakana = require("wanakana");
 const knex = require("./db/knex");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
 app.use(express.json()); //req.body
 app.use(cors());
@@ -232,14 +232,18 @@ app.get("/api/kanji/", async (req, res) => {
 
 app.get("/api/favorite", async (req, res) => {
   const favorite = req.query["options"];
-  await knex("favorite").insert({ kanji: favorite });
-  const countObj = await knex("favorite").where({kanji: favorite}).count("kanji");
-  const count = countObj[0]["count"];
-  if (count === 1) {
-    res.status(200).send(`You're the first person who selected ${favorite} !`);
-  } else {
-    res.status(200).send(`${count} people have chosen ${favorite} !`);
+  
+  if (favorite) {
+    await knex("favorite").insert({ kanji: favorite });
+    const countObj = await knex("favorite").where({kanji: favorite}).count("kanji");
+    const count = countObj[0]["count"];
+    if (count === 1) {
+      res.status(200).send(`You're the first person who selected ${favorite} !`);
+    } else {
+      res.status(200).send(`${count} people have chosen ${favorite} !`);
+    }
   }
+
   res.status(202).send()
 })
 
