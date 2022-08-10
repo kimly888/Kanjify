3a9f43d0e53a654b5398696283ea42bdb1400ba7
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-10 10:20:40 +0900

    Feat: Restart on local storage solution; store current faves.

--- client/src/App.js
+++ client/src/App.js
@@ -18,8 +18,24 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [isSubmitted, setSubmitted] = useState(false);
 
+  // ***** LOCAL STORAGE *****
+
+  // to run each time favourites is set
   useEffect(() => {
-    localStorage.setItem("favourites", JSON.stringify(favourites));
+    if (localStorage.getItem("favourites") === null) {
+      console.log("local storage empty, adding...");
+      localStorage.setItem("favourites", JSON.stringify(favourites));
+    } else {
+      console.log("local storage is not empty, updating...");
+      // 1. retrieve
+      const storedFavourites = JSON.parse(localStorage.getItem("favourites"));
+      // 2. add latest favourite to storedFavourites
+
+      // comparison for testing
+      console.log("storedFavourites: ", storedFavourites); // Array of Objects
+      console.log("regularFavourites: ", favourites); // Array of Objects
+      console.log("latestFavourite: ", favourites[favourites.length - 1]); // Object
+    }
   }, [favourites]);
 
   return (
