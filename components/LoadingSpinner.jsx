d1d0c67a14674652982afa199365caadd40cecdb
kimly.888 <kimly.jet@gmail.com>
2023-01-20 19:30:47 +0900

    Style: Change loading spinner

--- components/LoadingSpinner.jsx
+++ components/LoadingSpinner.jsx
@@ -2,8 +2,25 @@ import styles from "./LoadingSpinner.module.css";
 
 export default function LoadingSpinner() {
   return (
-    <div className={styles.spinningContainer}>
-      <div className={styles.loadingSpinner}></div>
-    </div>
+    <>
+      <div className={styles.loader}>
+        <div className={styles.dot}></div>
+      </div>
+      <div className={styles.loader}>
+        <div className={styles.dot}></div>
+      </div>
+      <div className={styles.loader}>
+        <div className={styles.dot}></div>
+      </div>
+      <div className={styles.loader}>
+        <div className={styles.dot}></div>
+      </div>
+      <div className={styles.loader}>
+        <div className={styles.dot}></div>
+      </div>
+      <div className={styles.loader}>
+        <div className={styles.dot}></div>
+      </div>
+    </>
   );
 }
