2f1684b744646dcb90742ac7d68c2c21221a6f28
kimly.888 <kimly.jet@gmail.com>
2023-01-31 11:40:09 +0900

    Chore: Remove Loading text

--- components/LoadingSpinner.jsx
+++ components/LoadingSpinner.jsx
@@ -3,7 +3,6 @@ import styles from "./LoadingSpinner.module.css";
 export default function LoadingSpinner() {
   return (
     <>
-      <p className={styles.text}>Loading...</p>
       <div className={styles.loader}>
         <div className={styles.dot}></div>
       </div>
