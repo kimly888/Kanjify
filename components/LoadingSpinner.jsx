ec44faa68b23e6c7372f5580282179595f3a2623
kimly.888 <kimly.jet@gmail.com>
2023-01-31 11:22:51 +0900

    Feat: Add mobile device media query for LoadingSpinner

--- components/LoadingSpinner.jsx
+++ components/LoadingSpinner.jsx
@@ -3,6 +3,7 @@ import styles from "./LoadingSpinner.module.css";
 export default function LoadingSpinner() {
   return (
     <>
+      <p className={styles.text}>Loading...</p>
       <div className={styles.loader}>
         <div className={styles.dot}></div>
       </div>
