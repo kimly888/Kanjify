a4a54b02f4d951a803811d5d082660b4dff2c34f
kimly.888 <kimly.jet@gmail.com>
2023-02-05 23:02:43 +0900

    Feat: Add open graph meta tags

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,21 @@ function App() {
         <title>Kanjify</title>
         <meta
           name="description"
-          content="Introducing Kanjify! Easily convert your name into Japanese and discover its meaning with just one click. "
+          content="Introducing Kanjify! Easily convert your name into Japanese and discover its meaning with just one click."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="UTF-8" />
+        <meta property="og:url" content="https://kanjify.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/90857923/183323486-118d6e93-6000-4c69-9623-94d48353ec9b.png"
+        />
+        <meta property="og:title" content="Kanjify" />
+        <meta property="og:site_name" content="Kanjify" />
+        <meta
+          property="og:description"
+          content="Introducing Kanjify! Easily convert your name into Japanese and discover its meaning with just one click."
+        />
         <meta
           name="keywords"
           content="kanji name, name in Japanese, kanji character, Japanese character, kanji symbols, japanese writing"
